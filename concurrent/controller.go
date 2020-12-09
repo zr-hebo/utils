b30ab0df02bd95d2af155f35ad0533e52d39fb70
xiaobiao <xiaobiao@kuaishou.com>
2020-12-09 18:22:40 +0800

    并发控制添加 running worker数管理

--- concurrent/controller.go
+++ concurrent/controller.go
@@ -1,38 +1,45 @@
 package concurrent
 
 import (
-	"context"
-	"fmt"
+    "context"
+    "fmt"
 )
 
 type ConController struct {
-	allowSize  int
-	workerChan chan struct{}
+    allowSize  int
+    runningNum int
+    workerChan chan struct{}
 }
 
 func NewConController(size int) (cc *ConController) {
-	return &ConController{
-		allowSize:  size,
-		workerChan: make(chan struct{}, size),
-	}
+    return &ConController{
+        allowSize:  size,
+        workerChan: make(chan struct{}, size),
+    }
 }
 
 func (cc *ConController) Acquire() {
-	cc.workerChan <- struct{}{}
+    cc.workerChan <- struct{}{}
+    cc.runningNum++
 }
 
 func (cc *ConController) Release() {
-	<-cc.workerChan
+    <-cc.workerChan
+    cc.runningNum--
+}
+
+func (cc *ConController) RunningNum() int {
+    return cc.runningNum
 }
 
 func (cc *ConController) Wait(ctx context.Context) (err error) {
-	for idx := 0; idx < cc.allowSize; idx++ {
-		select {
-		case <-ctx.Done():
-			err = fmt.Errorf("concurrent controller cancel wait for context calceled")
-			return
-		case cc.workerChan <- struct{}{}:
-		}
-	}
-	return
+    for idx := 0; idx < cc.allowSize; idx++ {
+        select {
+        case <-ctx.Done():
+            err = fmt.Errorf("concurrent controller cancel wait for context calceled")
+            return
+        case cc.workerChan <- struct{}{}:
+        }
+    }
+    return
 }
