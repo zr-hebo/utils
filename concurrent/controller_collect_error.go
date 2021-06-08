a688d9e8e73c8b752eedfc0c95cd6544b0377f25
xiaobiao <xiaobiao@kuaishou.com>
2021-06-08 22:29:39 +0800

    添加错误信息收集并发控制器

---
+++ concurrent/controller_collect_error.go
@@ -0,0 +1,69 @@
+package concurrent
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+)
+
+type ConControllerWithError struct {
+	allowSize      int
+	runningNum     int
+	lock           sync.Mutex
+	workerChan     chan struct{}
+	errorCollector error
+}
+
+func NewConControllerWithError(size int) (cc *ConControllerWithError) {
+	return &ConControllerWithError{
+		allowSize:  size,
+		workerChan: make(chan struct{}, size),
+	}
+}
+
+func (cce *ConControllerWithError) Acquire() {
+	cce.workerChan <- struct{}{}
+	cce.lock.Lock()
+	cce.runningNum++
+	cce.lock.Unlock()
+}
+
+func (cce *ConControllerWithError) CollectError(err error) {
+	if err == nil {
+		return
+	}
+
+	if cce.errorCollector == nil {
+		cce.errorCollector = err
+	} else {
+		cce.errorCollector = fmt.Errorf("%s; %s", cce.errorCollector, err.Error())
+	}
+}
+
+func (cce *ConControllerWithError) Release() {
+	<-cce.workerChan
+	cce.lock.Lock()
+	cce.runningNum--
+	cce.lock.Unlock()
+}
+
+func (cce *ConControllerWithError) RunningNum() int {
+	return cce.runningNum
+}
+
+func (cce *ConControllerWithError) Wait(ctx context.Context) {
+	ticker := time.NewTicker(time.Second)
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("concurrent controller cancel wait for context canceled")
+			return
+
+		case <-ticker.C:
+			if cce.runningNum == 0 {
+				return
+			}
+		}
+	}
+}
